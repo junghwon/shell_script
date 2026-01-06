ff929cb36c107221fa33a0b71ba4c521b48b4224
junghwon <kehnchang@hotmail.co.jp>
2026-01-06 21:08:59 +0900

    for

--- prog010.sh
+++ prog010.sh
@@ -1,7 +1,8 @@
-#! /bin/sh
+#! /bin/bash
 
-#for ((i=0 ; i<5 ; i++))
-for i in {1..5}
+#for ((i=1; i<5; i++))	# bash では C言語風の for 文が使えます
+#for i in {1..5}			# bash では {1..5} が展開されます
+for i in $(seq 1 5)		# ash でも動作します
 do
 	echo $i
 done
